5e805b7f92d3c4c0cce529f0630b1373bdc03fbb
Warrenn205 <warrenspencer41@gmail.com>
2026-01-16 09:16:09 -0600

    Add project files.

---
+++ dimeboard.client/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+    const testTite = "This is a testing page."
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>{testTite}</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
