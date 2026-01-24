63e0dacabda2aba95b1a468310cdd4e9384cd079
Warrenn205 <warrenspencer41@gmail.com>
2026-01-24 10:42:10 -0600

    Added Swagger API for RESTful APIs

--- DimeBoard.Server/Program.cs
+++ DimeBoard.Server/Program.cs
@@ -1,15 +1,28 @@
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "My API for DimeBoard",
+        Version = "v1",
+        Description = "The API for DimeBoard."
+    });
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
